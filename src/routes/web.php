3ad322ee6fccdead09557a85d1af325c11fb455e
ignitedcms <iamthwee@gmail.com>
2023-11-20 19:55:39 +0000

    Update web.php

--- src/routes/web.php
+++ src/routes/web.php
@@ -2,4 +2,13 @@
 
 
 use Ignitedcms\Ignitedcms\Http\Controllers\ContactController;
+use Ignitedcms\Ignitedcms\Http\Controllers\InstallController;
+
 Route::middleware('web')->get('contact', [ContactController::class, 'index']);
+
+
+Route::get('/installer', [InstallController::class, 'index']);
+Route::get('/installer/db', [InstallController::class, 'bar']);
+Route::get('/installer/terms', [InstallController::class, 'one']);
+Route::get('/installer/register', [InstallController::class, 'two']);
+Route::post('/installer/validate_form', [InstallController::class, 'validate_form']);
