94386092bb48348a7f5fb48d54bd04c2aaf627a2
ignitedcms <iamthwee@gmail.com>
2023-11-21 16:20:36 +0000

    global helpers

--- src/IgnitedcmsServiceProvider.php
+++ src/IgnitedcmsServiceProvider.php
@@ -21,7 +21,7 @@ class IgnitedcmsServiceProvider extends ServiceProvider
 
         if ($this->app->runningInConsole()) {
             $this->publishes([
-                __DIR__.'/../config/config.php' => config_path('ignitedcms.php'),
+                __DIR__.'/../config/app.php' => config_path('app.php'),
             ], 'config');
 
             // Publishing the views.
@@ -57,7 +57,7 @@ class IgnitedcmsServiceProvider extends ServiceProvider
     public function register()
     {
         // Automatically apply the package configuration
-        $this->mergeConfigFrom(__DIR__.'/../config/config.php', 'ignitedcms');
+        // $this->mergeConfigFrom(__DIR__.'/../config/config.php', 'ignitedcms');
 
         // Register the main class to use with the facade
         $this->app->singleton('ignitedcms', function () {
--- src/views/admin/dashboard/index.blade.php
+++ src/views/admin/dashboard/index.blade.php
@@ -6,7 +6,7 @@
         @include('ignitedcms::admin.sidebar')
 
         <div class="main-content " id="main-content">
-
+        {{ Helper::foo() }}
             <!--dashboard underneath-->
             <div class="p-3">
                 <div class="panel br drop-shadow">
