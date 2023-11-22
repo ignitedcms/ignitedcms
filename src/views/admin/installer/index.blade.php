80c5329d1a5d6a92ab71d229a9b366c4b6510da5
ignitedcms <iamthwee@gmail.com>
2023-11-22 16:03:14 +0000

    Update index.blade.php

--- src/views/admin/installer/index.blade.php
+++ src/views/admin/installer/index.blade.php
@@ -1,14 +1,25 @@
 @extends('ignitedcms::admin.layout')
 
 @section('content')
-    <div class="full-screen v-a h-a bg-light-grey">
-        <div class="container" id="app">
-            <img class="img-responsive" src="{{ asset('admin/images/logo.svg') }}"></img>
-            <div class="panel m-t-3 br drop-shadow text-center h-a v-a" style="height:150px; width:200px;">
-                <a href="{{ url('installer/terms') }}">
-                    <button type="button" class="btn btn-primary">Install</button>
-                </a>
+<div class="full-screen v-a h-a bg-light-grey">
+   <div class="container" id="app">
+      <div class="panel m-t-3 br drop-shadow" style="height:200px; width:300px;">
+         <div class="srow">
+            <div class="col-12 h-a">
+               <img class="img-responsive" width="200px;" src="{{ asset('admin/images/ignitedcms-logo.svg') }}"></img>
+
             </div>
-        </div>
-    </div>
+            
+            <div class="col-12 text-center">
+               <a href="{{ url('installer/terms') }}">
+                  <button type="button" class="btn btn-primary btn-block ">Install</button>
+               </a>
+            </div>
+            
+            
+         </div>
+
+      </div>
+   </div>
+</div>
 @endsection
