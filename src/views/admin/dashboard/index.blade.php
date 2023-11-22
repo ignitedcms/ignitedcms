e2f07540555bb6e9a592e4b75ab4cd2e38618355
ignitedcms <iamthwee@gmail.com>
2023-11-22 12:39:09 +0000

    u

--- src/views/admin/dashboard/index.blade.php
+++ src/views/admin/dashboard/index.blade.php
@@ -8,7 +8,7 @@
         <div class="main-content " id="main-content">
             <!--dashboard underneath-->
             <div class="p-3">
-                <div class="panel br drop-shadow">
+                <div class="panel br drop-shadow" style="min-height:400px;">
                     <h3>Dashboard</h3>
                     <p>This is where updates and important news related
                      to IgnitedCMS will be shown.
