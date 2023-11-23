d9ba4dd3b078ecdf323695571dd1fbac5bf7a767
ignitedcms <iamthwee@gmail.com>
2023-11-23 10:30:27 +0000

    u

---
+++ src/Http/Controllers/admin/DatabaseController.php
@@ -0,0 +1,43 @@
+<?php
+/*
+|---------------------------------------------------------------
+| Database controller
+|---------------------------------------------------------------
+|
+| Dumps MySQL db
+|
+|
+| @author: IgnitedCMS
+| @license: MIT
+| @version: 1.0
+| @since: 1.0
+*/
+
+namespace Ignitedcms\Ignitedcms\Http\Controllers\admin;
+
+use App\Http\Controllers\Controller;
+use Ignitedcms\Ignitedcms\Http\Middleware\Igs_auth;
+
+use Ignitedcms\Ignitedcms\Models\admin\Database;
+
+class Database extends Controller
+{
+   public function __construct() 
+   {
+      this->middleware(Igs_auth::class);
+   }
+
+    public function index()
+    {
+        $data = '';
+
+        return view('ignitedcms::admin.database.index')->with([
+            'data' => $data,
+        ]);
+    }
+
+    public static function backup()
+    {
+        Database::backup();
+    }
+}
