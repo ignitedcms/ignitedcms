d7b34440f03dd9ddcc414e9686e1a6f0f9da680e
ignitedcms <iamthwee@gmail.com>
2023-11-29 18:14:33 +0000

    Update index.blade.php

--- src/views/admin/dashboard/index.blade.php
+++ src/views/admin/dashboard/index.blade.php
@@ -8,12 +8,26 @@
         <div class="main-content " id="main-content">
             <!--dashboard underneath-->
             <div class="p-3">
-                <div class="panel br drop-shadow" style="min-height:400px;">
-                    <h3>Dashboard</h3>
-                    <p>This is where updates and important news related
-                     to IgnitedCMS will be shown.
-                  </p>
-                </div>
+               <div class="row">
+                  <div class="col">
+                     <h3>Dashboard</h3>
+                  </div>
+               </div>
+               <div class="row">
+                  <div class="col-6">
+                     <div class="panel br drop-shadow" style="min-height:400px;">
+                        <h3>Update</h3>
+                        <p>Watch here for the latest updates</p>
+                     </div>
+                  </div>
+                  <div class="col-6">
+                     <div class="panel br drop-shadow" style="min-height:300px;">
+                        <h3>Help</h3>
+                        <p>For all the latest help</p>
+                     </div>
+                  </div>
+               </div>
+                
 
             </div>
         </div>
