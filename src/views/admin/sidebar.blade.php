54d47c270e9daab367a4f6a5bdaea3eceb295823
ignitedcms <iamthwee@gmail.com>
2023-11-29 17:15:04 +0000

    Update sidebar.blade.php

--- src/views/admin/sidebar.blade.php
+++ src/views/admin/sidebar.blade.php
@@ -15,7 +15,7 @@
             <li class="m-t">
                <form action="{{ url('logout') }} " method="POST">
                   @csrf
-                  <button type="submit" class="btn btn-white">logout</button>
+                  <button type="submit" class="rm-btn-styles">logout</button>
                </form>
             </li>
         </ul>
@@ -36,7 +36,7 @@
             <li class="m-t">
                 <form action="{{ url('logout') }} " method="POST">
                    @csrf
-                  <button type="submit" class="btn btn-white">logout</button>
+                  <button type="submit" class="rm-btn-styles">logout</button>
                 </form>
             </li>
         </ul>
