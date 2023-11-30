94ef1b18a6dcc7ef59b7ac33bc0b5c44aacb431f
ignitedcms <iamthwee@gmail.com>
2023-11-30 13:08:59 +0000

    Update create.blade.php

--- src/views/admin/matrix/create.blade.php
+++ src/views/admin/matrix/create.blade.php
@@ -15,14 +15,29 @@
                     </div>
                     <div class="breadcrumb-item">Create matrix</div>
                 </div>
+
+                
+
+                <div class="alert alert-info m-b-3">
+                  The matrix is a special field where you can
+                  create repeatable content in your section types.
+                  <br/><br/> For more information please refer to the documentation.
+                </div>
+
+                <div class="row">
+                   <div class="col-12 right">
+                      <button type="submit" class="btn btn-primary">Save matrix</button>
+                   </div>
+                </div>
                 <!--main part for section styles -->
                 <div class="panel br drop-shadow">
+                  <h3>Matrix</h3>
                    le matrix
                 </div>
 
                 <div class="row">
                     <div class="col-12 right">
-                        <button type="submit" class="btn btn-white">Save matrix</button>
+                        <button type="submit" class="btn btn-primary">Save matrix</button>
                     </div>
                 </div>
 
