e91a89b251904d9ee01e9d9270a5c14ca525ad9d
ignitedcms <iamthwee@gmail.com>
2023-11-30 15:16:21 +0000

    Update create.blade.php

--- src/views/admin/matrix/create.blade.php
+++ src/views/admin/matrix/create.blade.php
@@ -30,22 +30,27 @@
             <!--main part for section styles -->
             <div class="panel br drop-shadow">
                 <h3>Matrix</h3>
-                <div class="form-group">
-                    <label for="title">Matrix field name <span class="text-danger">*</span></label>
-                    <div class="small text-muted m-b">What this field will be called in the control panel.This MUST be
-                        unique and must
-                        not contain numbers or spaces. You can NOT use the following reserved names:
-                        [url,content,id,section,field,entrytitle,entryid]</div>
-                    <input class="form-control" name="matrix_name" value="" placeholder="Start typing" />
+                <div class="row">
+                   <div class="col">
+                      <div class="form-group">
+                         <label for="title">Matrix field name <span class="text-danger">*</span></label>
+                         <div class="small text-muted m-b">What this field will be called in the control panel.This MUST be
+                            unique and must
+                            not contain numbers or spaces. You can NOT use the following reserved names:
+                            [url,content,id,section,field,entrytitle,entryid]</div>
+                         <input class="form-control" name="matrix_name" value="" placeholder="Start typing" />
+                      </div>
+                   </div>
+                   
                 </div>
+                
                 <div class="divider"></div>
 
-                <div class="col-sm-12">
-                    <strong>Configuration</strong> <br />
-                    <div class="igs-small">Define the types of blocks that can be created within this Matrix field, as well
+                <div class="row m-t-2">
+                    <h4>Configuration</h4> 
+                    <div class="small text-muted m-b-3">Define the types of blocks that can be created within this Matrix field, as well
                         as the fields each block type is made up of.
                     </div>
-                    <br />
                     <!-- start matrix container -->
                     <div class="matrix-container">
                         <div class="matrix-left">
@@ -64,26 +69,23 @@
                             <div class="matrix-right-container">
                                 <div class="form-group">
                                     <label>Field Name</label>
-                                    <div class="errors pull-left">*</div>
-                                    <div class="igs-small">(This cannot be empty and must be unique and contain no spaces or
+                                    <div class="small text-muted">(This cannot be empty and must be unique and contain no spaces or
                                         numbers)
                                     </div>
-                                    <input v-model="fieldname" type="text" class="form-control" placeholder="Type here"
-                                        data-toggle="tooltip" data-placement="top" value="">
+                                    <input v-model="fieldname" type="text" class="form-control" 
+                                         value="">
                                 </div>
                                 <div class="form-group">
                                     <label>Instruction text</label>
-                                    <div class="errors pull-left">*</div>
-                                    <div class="igs-small">(Text instructions for user, optional)
+                                    <div class="small text-muted">(Text instructions for user, optional)
                                     </div>
                                     <input v-model="instructions" type="text" class="form-control"
-                                        placeholder="Type here" data-toggle="tooltip" data-placement="top" value="">
+                                        placeholder="Type here"  value="">
                                 </div>
                                 <div class="form-group">
                                     <label>Field Type</label>
-                                    <div class="errors pull-left">*</div>
-                                    <div class="igs-small">Specify the field type</div>
-                                    <select class="form-control m-b" id="type" v-model="crselect">
+                                    <div class="small text-muted">Specify the field type</div>
+                                    <select class="form-select m-b" id="type" v-model="crselect">
                                         <option value="plain-text" selected>Plain Text</option>
                                         <option value="multi-line">Multi-line Box</option>
                                         <option value="rich-text">Rich Text Box</option>
@@ -100,9 +102,9 @@
                                 <div v-if="crselect=='plain-text'">
                                     <div class="form-group">
                                         <label>Character length</label>
-                                        <div class="igs-small">(Must be valid whole number)</div>
+                                        <div class="small text-muted">(Must be valid whole number)</div>
                                         <input v-model="fieldlength" type="text" class="form-control"
-                                            placeholder="Type here" data-toggle="tooltip" data-placement="top"
+                                            placeholder="Type here" 
                                             value="">
                                     </div>
                                 </div>
@@ -113,17 +115,16 @@
                                 <div v-if="crselect=='drop-down'">
                                     <div class="form-group">
                                         <label>Options</label>
-                                        <div class="igs-small">Please separate with commas</div>
-                                        <textarea v-model="variations" class="form-control" rows="5" data-maxlength="this" data-required="true"
-                                            placeholder="Type here" data-toggle="tooltip" data-placement="top" title=""></textarea>
+                                        <div class="small text-muted">Please separate with commas</div>
+                                        <textarea v-model="variations" class="form-control" rows="5" placeholder="Type here" ></textarea>
                                     </div>
                                 </div>
                                 <div v-if="crselect=='check-box'">
                                     <div class="form-group">
                                         <label>Options</label>
-                                        <div class="igs-small">Please separate with commas</div>
-                                        <textarea v-model="variations" class="form-control" rows="5" data-maxlength="this" data-required="true"
-                                            placeholder="Type here" data-toggle="tooltip" data-placement="top" title=""></textarea>
+                                        <div class="small text-muted">Please separate with commas</div>
+                                        <textarea v-model="variations" class="form-control" rows="5" 
+                                            placeholder="Type here"></textarea>
                                     </div>
                                 </div>
                                 <div v-if="crselect=='color'">
@@ -133,8 +134,7 @@
                                         <label>Allowed File types</label>
                                         <div class="igs-small"></div>
                                         <input v-model="variations" type="text" class="form-control"
-                                            placeholder="jpg,png,gif" data-toggle="tooltip" data-placement="top"
-                                            value="">
+                                            placeholder="jpg,png,gif" value="">
                                     </div>
                                 </div>
                                 <div v-if="crselect=='number'">
@@ -154,9 +154,10 @@
                     <!-- end matrix container -->
                     <pre> matrixContent </pre>
                 </div>
-                <div class="col-sm-12">
-                    <div class="form-group">
-                        <div class="btn btn-purplet pull-right btn-s-xs" @click="save"><strong>Save All</strong></div>
+
+                <div >
+                    <div class="form-group right">
+                        <div class="btn btn-primary " @click="save"><strong>Save All</strong></div>
                     </div>
                 </div>
 
@@ -164,11 +165,7 @@
 
             </div>
 
-            <div class="row">
-                <div class="col-12 right">
-                    <button type="submit" class="btn btn-primary">Save matrix</button>
-                </div>
-            </div>
+            
 
             <div class="gap"></div>
             <!--end main part-->
