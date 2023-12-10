92b98529c5ef2000dec51eb073a3580347a64f10
ignitedcms <iamthwee@gmail.com>
2023-12-10 21:41:30 +0000

    u

--- tests/Feature/PermissionTest.php
+++ tests/Feature/PermissionTest.php
@@ -4,7 +4,6 @@ namespace Ignitedcms\Ignitedcms\Tests;
 
 class PermissionTest extends TestCase
 {
-
     public function test_permissions_dashboard()
     {
         //$this->withoutExceptionHandling();
@@ -18,12 +17,11 @@ class PermissionTest extends TestCase
     public function test_cannot_delete_admin_permission()
     {
         $response = $this->withSession(['logged_in' => 1, 'userid' => '1'])
-           ->post('admin/permissions/delete/1');
+            ->post('admin/permissions/delete/1');
 
         $this->assertDatabaseHas('permission_groups', [
-            'groupName' => 'Administrators'
+            'groupName' => 'Administrators',
         ]);
 
     }
-
 }
