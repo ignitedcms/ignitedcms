540290d67c414a6f70688260b6686a71be01c7b3
ignitedcms <iamthwee@gmail.com>
2023-12-12 13:48:31 +0000

    Update migration.php

--- src/public/migration.php
+++ src/public/migration.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('assetfields', function (Blueprint $table) {
+         Schema::create('assetfields', function (Blueprint $table) {
             $table->increments('id');
             $table->integer('entryid')->nullable();
             $table->string('filename', 255)->nullable();
@@ -28,51 +28,51 @@ return new class extends Migration
 
         Schema::create('routes', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('route', 200);
-            $table->string('controller', 200);
+            $table->string('route', 200)->nullable();
+            $table->string('controller', 200)->nullable();
             $table->timestamps();
         });
 
         Schema::create('plugins', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name', 255);
-            $table->dateTime('install');
-            $table->integer('status');
+            $table->string('name', 255)->nullable();
+            $table->dateTime('install')->nullable();
+            $table->integer('status')->nullable();
             $table->timestamps();
         });
 
         Schema::create('cat_links', function (Blueprint $table) {
             $table->increments('id');
-            $table->integer('prod_id');
-            $table->integer('cat_id');
+            $table->integer('prod_id')->nullable();
+            $table->integer('cat_id')->nullable();
             $table->timestamps();
         });
 
          Schema::create('cats', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('cat_name', 100);
+            $table->string('cat_name', 100)->nullable();
             $table->timestamps();
         });
 
         Schema::create('content', function (Blueprint $table) {
             $table->increments('id');
-            $table->integer('entryid');
-            $table->string('entrytitle', 200);
+            $table->integer('entryid')->nullable();
+            $table->string('entrytitle', 200)->nullable();
             $table->timestamps();
         });
 
          Schema::create('entry', function (Blueprint $table) {
             $table->increments('id');
-            $table->integer('sectionid');
-            $table->string('type', 200);
-            $table->date('datecreated');
-            $table->integer('sort_order');
+            $table->integer('sectionid')->nullable();
+            $table->string('type', 200)->nullable();
+            $table->date('datecreated')->nullable();
+            $table->integer('sort_order')->nullable();
             $table->timestamps();
         });
 
          Schema::create('fields', function (Blueprint $table) {
-            $table->string('name', 200);
-            $table->string('type', 50);
+            $table->string('name', 200)->nullable();
+            $table->string('type', 50)->nullable();
             $table->text('opts')->nullable();
             $table->string('instructions', 200)->nullable();
             $table->string('maxchars', 50)->nullable();
@@ -98,90 +98,86 @@ return new class extends Migration
 
         Schema::create('permissions', function (Blueprint $table) {
             $table->increments('permissionID');
-            $table->string('permission', 200);
-            $table->integer('order_position');
+            $table->string('permission', 200)->nullable();
+            $table->integer('order_position')->nullable();
             $table->timestamps();
         });
 
 
         Schema::create('section', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name', 100);
-            $table->string('sectiontype', 200);
+            $table->string('name', 100)->nullable();
+            $table->string('sectiontype', 200)->nullable();
             $table->timestamps();
         });
 
         Schema::create('section_layout', function (Blueprint $table) {
             $table->increments('s_id');
-            $table->integer('sectionid');
-            $table->integer('fieldid');
-            $table->integer('sortorder');
-            $table->integer('required');
+            $table->integer('sectionid')->nullable();
+            $table->integer('fieldid')->nullable();
+            $table->integer('sortorder')->nullable();
+            $table->integer('required')->nullable();
             $table->timestamps();
         });
 
         Schema::create('blocks', function (Blueprint $table) {
             $table->increments('id');
-            $table->integer('fieldid');
-            $table->integer('typeid');
-            $table->integer('sortorder');
-            $table->dateTime('datecreated');
+            $table->integer('fieldid')->nullable();
+            $table->integer('typeid')->nullable();
+            $table->integer('sortorder')->nullable();
+            $table->dateTime('datecreated')->nullable();
             $table->timestamps();
         });
 
          Schema::create('user', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name', 100);
-            $table->string('password', 500);
-            $table->date('joindate');
-            $table->integer('logins');
-            $table->integer('is_logged_in');
-            $table->string('email', 50);
-            $table->integer('activ_status');
-            $table->string('activ_key', 1000);
-            $table->integer('permissiongroup');
-            $table->string('fullname', 200);
+            $table->string('name', 100)->nullable();
+            $table->string('password', 500)->nullable();
+            $table->date('joindate')->nullable();
+            $table->integer('logins')->nullable();
+            $table->integer('is_logged_in')->nullable();
+            $table->string('email', 50)->nullable();
+            $table->integer('activ_status')->nullable();
+            $table->string('activ_key', 1000)->nullable();
+            $table->integer('permissiongroup')->nullable();
+            $table->string('fullname', 200)->nullable();
             $table->timestamps();
         });
 
         Schema::create('paypal', function (Blueprint $table) {
             $table->increments('id');
-            $table->integer('sandbox');
-            $table->string('paypal_email', 100);
-            $table->string('notify_url', 512);
-            $table->string('thanks_url', 512);
-            $table->string('currency_code', 10);
-            $table->dateTime('created_at');
-            $table->dateTime('updated_at');
+            $table->integer('sandbox')->nullable();
+            $table->string('paypal_email', 100)->nullable();
+            $table->string('notify_url', 512)->nullable();
+            $table->string('thanks_url', 512)->nullable();
+            $table->string('currency_code', 10)->nullable();
             $table->timestamps();
         });
 
         Schema::create('ipn', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('item_name', 512);
-            $table->string('item_number', 512);
-            $table->string('payment_status', 512);
-            $table->string('payment_amount', 512);
-            $table->string('payment_currency', 512);
-            $table->string('txn_id', 512);
-            $table->string('receiver_email', 512);
-            $table->string('payer_email', 512);
-            $table->string('txn_type', 512);
-            $table->string('pending_reason', 512);
-            $table->string('payment_type', 512);
-            $table->string('custom', 512);
-            $table->string('invoice', 512);
-            $table->string('first_name', 512);
-            $table->string('last_name', 512);
-            $table->string('address_name', 512);
-            $table->string('address_country', 512);
-            $table->string('address_country_code', 512);
-            $table->string('address_zip', 512);
-            $table->string('address_state', 512);
-            $table->string('address_city', 512);
-            $table->string('address_street', 512);
-            $table->dateTime('created_at');
-            $table->dateTime('updated_at');
+            $table->string('item_name', 512)->nullable();
+            $table->string('item_number', 512)->nullable();
+            $table->string('payment_status', 512)->nullable();
+            $table->string('payment_amount', 512)->nullable();
+            $table->string('payment_currency', 512)->nullable();
+            $table->string('txn_id', 512)->nullable();
+            $table->string('receiver_email', 512)->nullable();
+            $table->string('payer_email', 512)->nullable();
+            $table->string('txn_type', 512)->nullable();
+            $table->string('pending_reason', 512)->nullable();
+            $table->string('payment_type', 512)->nullable();
+            $table->string('custom', 512)->nullable();
+            $table->string('invoice', 512)->nullable();
+            $table->string('first_name', 512)->nullable();
+            $table->string('last_name', 512)->nullable();
+            $table->string('address_name', 512)->nullable();
+            $table->string('address_country', 512)->nullable();
+            $table->string('address_country_code', 512)->nullable();
+            $table->string('address_zip', 512)->nullable();
+            $table->string('address_state', 512)->nullable();
+            $table->string('address_city', 512)->nullable();
+            $table->string('address_street', 512)->nullable();
             $table->timestamps();
         });
 
@@ -193,8 +189,7 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('personal_access_tokens');
+        Schema::dropIfExists('tables');
     }
 };
-
 
