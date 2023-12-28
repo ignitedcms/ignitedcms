dfef897e25a78ee41f950ec5d75e35d28d533634
ignitedcms <iamthwee@gmail.com>
2023-12-28 11:15:14 +0000

    Update edit.blade.php

--- src/views/admin/entry/edit.blade.php
+++ src/views/admin/entry/edit.blade.php
@@ -5,15 +5,15 @@
 
         <div class="main-content p-3" id="main-content">
 
-           <drawer title="Help">
-              <div class="p-3">
-                 <h4>Templating</h4>
-                 <p class="text-muted">For more help please see</p>
-                 <a href="https://www.ignitedcms.com/documentation/section-types" target="_blank">Templating</a>
-                  <br>
-                 <a href="https://www.ignitedcms.com/documentation/magic-routing">Enable magic routing</a>
-              </div>
-           </drawer>
+            <drawer title="Help">
+                <div class="p-3">
+                    <h4>Templating</h4>
+                    <p class="text-muted">For more help please see</p>
+                    <a href="https://www.ignitedcms.com/documentation/section-types" target="_blank">Templating</a>
+                    <br>
+                    <a href="https://www.ignitedcms.com/documentation/magic-routing">Enable magic routing</a>
+                </div>
+            </drawer>
 
             @if (Helper::isMultiple($sectionid) == true)
                 <div class="breadcrumb m-b-3">
@@ -49,7 +49,7 @@
 
                 <div class="row">
                     <div class="col-3 v-a">
-                     
+
 
                         @if (Helper::isSingle($sectionid) == true)
                             <modal button-title="Create Template" modal-header="Create single template">
@@ -89,16 +89,13 @@
                 </div>
                 <div class="panel br drop-shadow">
 
-                     @include('ignitedcms::admin.entry.vue')
+                    @include('ignitedcms::admin.entry.vue')
 
                     @if (Helper::isMultiple($sectionid) == true)
-                  
-
                         <label for="entry title">Entry title</label>
                         <div class="small text-muted">This is a required field*</div>
-                        <input class="form-control" name="entrytitle" 
-                           value="{{ Helper::getContent($entryid, 'entrytitle') }}"
-                           disabled />
+                        <input class="form-control" name="entrytitle"
+                            value="{{ Helper::getContent($entryid, 'entrytitle') }}" disabled />
 
                         @error('entrytitle')
                             <div class="small text-danger">{{ $message }}</div>
@@ -110,7 +107,7 @@
                     @foreach ($data as $row)
                         <div>
                             @if ($row->type == 'plain-text')
-                               <div class="clearfix m-b-2"></div>
+                                <div class="clearfix m-b-2"></div>
                                 <label for="title">[{{ $row->name }}]</label>
                                 <div class="small text-muted">{{ $row->instructions }}</div>
                                 <input class="form-control" name="{{ $row->name }}"
@@ -120,36 +117,36 @@
                                 <div class="divider m-b-2"></div>
                             @elseif ($row->type == 'matrix')
                                 <div @click="goNow({{ $row->id }})" class="m-r-2 m-t btn btn-white ">
-                                 <span class="v-a">
-                                   <i data-feather="plus"></i>    
-                                   Add {{ $row->name }}
-                                </span>
-                                 </div>  
-                              <input style='display:none;' type='text' name='{{ $row->name }}'  
-                                 id='{{ $row->name }}' v-model='JSON.stringify(matrix)' />
-
-
+                                    <span class="v-a">
+                                        <i data-feather="plus"></i>
+                                        Add {{ $row->name }}
+                                    </span>
+                                </div>
+                                <input style='display:none;' type='text' name='{{ $row->name }}'
+                                    id='{{ $row->name }}' v-model='JSON.stringify(matrix)' />
                             @elseif ($row->type == 'multi-line')
-                               <div class="clearfix m-b-2"></div>
+                                <div class="clearfix m-b-2"></div>
                                 <label for="title">[{{ $row->name }}]</label>
                                 <div class="small text-muted">{{ $row->instructions }}</div>
                                 <textarea class="form-control" name="{{ $row->name }}" placeholder="Start typing" rows="4">{{ Helper::getContent($entryid, $row->name) }}</textarea>
                                 {{ $row->formvalidation }}
                                 <div class="divider m-b-2"></div>
                             @elseif ($row->type == 'rich-text')
-                               <div class="clearfix m-b-2"></div>
-                             
-                                 <div v-for='part in singleRichtext'>
-                                    <div class='form-group' v-if='part.name == "{{$row->name}}"'>
-                                       <label>[{{ $row->name }}]</label>
-                                       <div class='igs-small'>{{ $row->instructions }}</div>
-                                       <input name="{{$row->name}}" v-bind:value='part.content' style='display:none;'></input>
+                                <div class="clearfix m-b-2"></div>
 
-                                       <quill-editor v-model='part.content' :options='editorOption' @blur='onEditorBlur($event)'
-                                                     @focus='onEditorFocus($event)' @ready='onEditorReady($event)'>
-                                       </quill-editor>
+                                <div v-for='part in singleRichtext'>
+                                    <div class='form-group' v-if='part.name == "{{ $row->name }}"'>
+                                        <label>[{{ $row->name }}]</label>
+                                        <div class='igs-small'>{{ $row->instructions }}</div>
+                                        <input name="{{ $row->name }}" v-bind:value='part.content'
+                                            style='display:none;'></input>
+
+                                        <quill-editor v-model='part.content' :options='editorOption'
+                                            @blur='onEditorBlur($event)' @focus='onEditorFocus($event)'
+                                            @ready='onEditorReady($event)'>
+                                        </quill-editor>
                                     </div>
-                                 </div>
+                                </div>
 
                                 <div class="form-group">
 
@@ -157,7 +154,7 @@
                                 {{ $row->formvalidation }}
                                 <div class="divider m-b-2"></div>
                             @elseif ($row->type == 'check-box')
-                               <div class="clearfix m-b-2"></div>
+                                <div class="clearfix m-b-2"></div>
                                 <label for="title">[{{ $row->name }}]</label>
                                 <div class="small text-muted">{{ $row->instructions }}</div>
                                 <div>
@@ -169,7 +166,7 @@
                                 {{ $row->formvalidation }}
                                 <div class="divider m-b-2"></div>
                             @elseif ($row->type == 'color')
-                               <div class="clearfix m-b-2"></div>
+                                <div class="clearfix m-b-2"></div>
                                 <label for="title">[{{ $row->name }}]</label>
                                 <div class="small text-muted">{{ $row->instructions }}</div>
                                 <input class="form-control" type="color" name="{{ $row->name }}"
@@ -177,30 +174,31 @@
                                 {{ $row->formvalidation }}
                                 <div class="divider m-b-2"></div>
                             @elseif ($row->type == 'drop-down')
-                               <div class="clearfix m-b-2"></div>
+                                <div class="clearfix m-b-2"></div>
                                 <label for="title">[{{ $row->name }}]</label>
                                 <div class="small text-muted">{{ $row->instructions }}</div>
 
-                                <select class="form-select" name="{{ $row->name }}" aria-label="Default select example">
+                                <select class="form-select" name="{{ $row->name }}"
+                                    aria-label="Default select example">
                                     {{ Helper::buildDropdown($entryid, $row->name) }}
                                 </select>
                                 {{ $row->formvalidation }}
                                 <div class="divider m-b-2"></div>
                             @elseif ($row->type == 'file-upload')
-                               <div class="clearfix m-b-2"></div>
+                                <div class="clearfix m-b-2"></div>
                                 <label for="title">[{{ $row->name }}]</label>
                                 <div class="small text-muted">{{ $row->instructions }}</div>
-                                 
-                                 <asset-container fieldname2='{{ $row->name }}' 
-                                    assetid='{{ Helper::getContent($entryid, $row->name) }}' 
-                                       url='{{ Helper::getThumb($entryid, $row->name) }}'> 
-                                 </asset-container>
-                                                                 
+
+                                <asset-container fieldname2='{{ $row->name }}'
+                                    assetid='{{ Helper::getContent($entryid, $row->name) }}'
+                                    url='{{ Helper::getThumb($entryid, $row->name) }}'>
+                                </asset-container>
+
 
                                 {{ $row->formvalidation }}
                                 <div class="divider m-b-2"></div>
                             @elseif ($row->type == 'number')
-                               <div class="clearfix m-b-2"></div>
+                                <div class="clearfix m-b-2"></div>
                                 <label for="title">[{{ $row->name }}]</label>
                                 <div class="small text-muted">{{ $row->instructions }}</div>
                                 <input class="form-control" name="{{ $row->name }}"
@@ -208,7 +206,7 @@
                                 {{ $row->formvalidation }}
                                 <div class="divider m-b-2"></div>
                             @elseif ($row->type == 'date')
-                               <div class="clearfix m-b-2"></div>
+                                <div class="clearfix m-b-2"></div>
                                 <label for="title">[{{ $row->name }}]</label>
                                 <div class="small text-muted">{{ $row->instructions }}</div>
                                 <div class="form-group">
@@ -219,7 +217,7 @@
                                 {{ $row->formvalidation }}
                                 <div class="divider m-b-2"></div>
                             @elseif ($row->type == 'switch')
-                               <div class="clearfix m-b-2"></div>
+                                <div class="clearfix m-b-2"></div>
                                 <label for="title">[{{ $row->name }}]</label>
                                 <div class="small text-muted">{{ $row->instructions }}</div>
                                 <switch-ios name="{{ $row->name }}"
@@ -235,5 +233,5 @@
             <div class="gap"></div>
         </div>
     </div>
-
 @endsection
+
