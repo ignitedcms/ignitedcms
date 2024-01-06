558e2206292882b564eb086bf6f3b95f003a4e68
ignitedcms <iamthwee@gmail.com>
2024-01-06 12:43:12 +0000

    Update FieldsTest.php

--- tests/Feature/FieldsTest.php
+++ tests/Feature/FieldsTest.php
@@ -17,4 +17,47 @@ class FieldsTest extends TestCase
 
         $response->assertSee('Fields');
     }
+
+    public function test_insert_field()
+    {
+       $response = $this->withSession([
+          'logged_in' => 1, 
+          'userid' => '1'
+       ])
+            ->post('admin/fields/create', [
+                'name' => 'foo',
+                'instructions' => 'foo',
+                'type' => 'plain-text',
+                'length' => '100',
+                'variations' => '',
+            ]);
+
+
+        $this->assertDatabaseHas('fields', [
+            'name' => 'foo',
+        ]);
+    }
+
+    /*                                                                          
+    |---------------------------------------------------------------            
+    | Need to figure out how to test ajax
+    |---------------------------------------------------------------            
+    */       
+    public function test_insert_field_noname()
+    {
+       //$response = $this->withSession([
+          //'logged_in' => 1, 
+          //'userid' => '1'
+       //])
+            //->post('admin/fields/create', [
+                //'name' => '',
+                //'instructions' => 'foo',
+                //'type' => 'plain-text',
+                //'length' => '100',
+                //'variations' => '',
+            //]);
+
+      //$response->assertSessionHasErrors('name');
+
+    }
 }
