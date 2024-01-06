535fc1af87a94f50eaa4815ab0d7e51d8fc7a494
ignitedcms <iamthwee@gmail.com>
2024-01-06 11:40:26 +0000

    Create ProfileTest.php

---
+++ tests/Feature/ProfileTest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Ignitedcms\Ignitedcms\Tests;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class ProfileTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function test_profile_page()
+    {
+        //$this->withoutExceptionHandling();
+
+        $response = $this->withSession(['logged_in' => 1, 'userid' => '1'])
+            ->get('admin/profile');
+
+        $response->assertSee('Profile');
+
+    }
+
+    public function test_profile_update_fullname()
+    {
+        $response = $this->withSession(['logged_in' => 1, 'userid' => '1'])
+            ->post('admin/profile/update', [
+                'fullname' => 'john doe',
+            ]);
+
+        //$response->assertRedirect('admin/users');
+
+        $this->assertDatabaseHas('user', [
+            'fullname' => 'john doe',
+        ]);
+
+    }
+}
