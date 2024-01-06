6a77723c1165436a53682670bc385a7081aabd71
ignitedcms <iamthwee@gmail.com>
2024-01-06 12:08:04 +0000

    Update SectionsTest.php

--- tests/Feature/SectionsTest.php
+++ tests/Feature/SectionsTest.php
@@ -7,4 +7,14 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 class SectionsTest extends TestCase
 {
     use RefreshDatabase;
+    public function test_sections_page()
+    {
+        //$this->withoutExceptionHandling();
+
+        $response = $this->withSession(['logged_in' => 1, 'userid' => '1'])
+            ->get('admin/section');
+
+        $response->assertSee('Sections');
+    }
+
 }
