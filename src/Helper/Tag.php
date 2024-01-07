8dfbdaa3d2222ced32fe7b7a5914b31c3803a91c
ignitedcms <iamthwee@gmail.com>
2024-01-07 11:03:09 +0000

    Update Tag.php

--- src/Helper/Tag.php
+++ src/Helper/Tag.php
@@ -1,8 +1,28 @@
 <?php    
+/*                                                                          
+|---------------------------------------------------------------            
+| Global helper functions
+|---------------------------------------------------------------            
+|
+| Used in various files controllers and views
+|
+*/       
 use Illuminate\Support\Facades\DB;
 
-if (!function_exists('ha')) {
-    function ha() {
+if (!function_exists('validSelect')) {
+    function validSelect($array) {
 
+        $ar = [];
+        foreach ($array as $var) {
+            array_push($ar, $var->option);
+        }
+
+        $a = self::noDuplicates($ar);
+        $b = self::validVariableNames($ar);
+        if ($a && $b) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
