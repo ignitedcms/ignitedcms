8f8f4e87571817b6175b968dcacfd51a6b195402
ignitedcms <iamthwee@gmail.com>
2024-01-10 12:29:04 +0000

    Update index.blade.php

--- src/views/admin/chunks/index.blade.php
+++ src/views/admin/chunks/index.blade.php
@@ -7,26 +7,41 @@
 
         <div class="main-content " id="main-content">
 
-        <div class="p-3">
-            <div class="row">
-                <div class="col-md-12">
-                    <div class="card w-50 m-auto">
-                        <div class="card-header bg-info text-white">
-                            <h4>Chunked uploader</h4>
-                        </div>
-                        <div class="card-body">
-                            <div class="form-group" id="file-input">
-                                <input type="file" id="pickfiles" class="form-control">
-                                <div id="filelist"></div>
-                            </div>
-                            <div class="form-group">
-                                <a id="upload" href="javascript:;" class="button">Upload file</a>
-                            </div>
-                        </div>
+           
+
+           <div class="p-3">
+
+              <div class="breadcrumb m-b-3">
+                 <div class="breadcrumb-item">
+                    <a href="{{ url('admin/dashboard') }}">Dashboard</a>
+                 </div>
+                 <div class="breadcrumb-item">Large file uploader</div>
+              </div>             
+
+              <div class="alert alert-success">
+                 <div class="text-black">Information</div>
+                 <div class="small text-muted">
+                    Upload large files here
+                 </div>
+              </div>
+              <div class="m-b-3"></div>
+              <div class="row panel br drop-shadow">
+                 <div class="col">
+                    <div class="form-group">
+                       <h4>Large file uploader</h4>
                     </div>
-                </div>
-            </div>
-        </div>
+
+                    <div class="form-group" id="file-input">
+                       <input type="file" id="pickfiles" class="form-control">
+                       <div id="filelist" class="m-t-2 m-b-2"></div>
+                    </div>
+                    <div class="form-group">
+                       <a id="upload" href="javascript:;" class="button">Upload file</a>
+                    </div>
+
+                 </div>
+              </div>
+           </div>
       </div>
    </div>
 
