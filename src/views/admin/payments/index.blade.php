ce355972067f01ab90de1c653d0a703e9de290d9
ignitedcms <iamthwee@gmail.com>
2024-01-13 09:58:51 +0000

    Update index.blade.php

--- src/views/admin/payments/index.blade.php
+++ src/views/admin/payments/index.blade.php
@@ -46,10 +46,96 @@
                   </div>
                </div>
                
-               
+               <tabs>
+               <tab-item title="Payment options">
+                  <div class="p-2">
+                     <div class="form-group">
+                        <div class="p-2 bg-white b br">
+                           <div class="row">
+                              <div class="col no-margin">
+                                 <div class="row">
+                                    <div class="col no-margin">
+                                       <div class="text-black">Enable Stripe</div>
+                                    </div>
+                                 </div>
+                                 <div class="row">
+                                    <div class="col no-margin">
+                                       <div class="small text-muted">
+                                          Please install Laravel cashier first
+                                       </div>
+                                    </div>
+                                 </div>
+                              </div>
+                              <div class="col no-margin right">
+                                 <div>
+                                    <label for="title"></label>
+                                    <div class="m-b"></div>
+                                    <div>
+                                       <div class="m-b"></div>
+                                       <switch-ios name="stripe" value=""></switch-ios>
+                                       
+                                    </div>
+                                 </div>
+                              </div>
+                           </div>
+                        </div>
+                     </div>
+                     <div class="form-group">
+                        <div class="p-2 bg-white b br">
+                           <div class="row">
+                              <div class="col no-margin">
+                                 <div class="row">
+                                    <div class="col no-margin">
+                                       <div class="text-black">Enable Paypal</div>
+                                    </div>
+                                 </div>
+                                 <div class="row">
+                                    <div class="col no-margin">
+                                       <div class="small text-muted">
+                                          Paypal via IPN only supported
+                                       </div>
+                                    </div>
+                                 </div>
+                              </div>
+                              <div class="col no-margin right">
+                                 <div>
+                                    <label for="title"></label>
+                                    <div class="m-b"></div>
+                                    <div>
+                                       <div class="m-b"></div>
+                                          <switch-ios name="paypal" value="checked"></switch-ios>
+                                    </div>
+                                 </div>
+                              </div>
+                           </div>
+                        </div>
+                     </div>
+                     <div class="form-group right">
+                        <button type="submit" class="btn btn-primary">Save</button>
+                     </div>
+
+                  </div>
+               </tab-item>
+               <tab-item title="Paypal settings">
+                  <div class="p-2">
+                     <p>
+
+                     </p>
+                  </div>
+               </tab-item>
+               <tab-item title="Stripe settings">
+                  <div class="p-2">
+                     <p>
+
+                     </p>
+                  </div>
+               </tab-item>
+               </tabs>
+
 
             </div>
 
+            <div class="gap"></div>
         </div>
 
     </div>
