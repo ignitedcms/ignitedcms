49fca4d530782f069209d258e0bc8cc9e5821a74
ignitedcms <iamthwee@gmail.com>
2024-01-27 19:06:33 +0000

    remove payment gateway
    ...for now

--- src/views/admin/sidebar.blade.php
+++ src/views/admin/sidebar.blade.php
@@ -31,9 +31,6 @@
          @if (sidebarGuard(3))
             <li class="m-t"><a href="{{ url('admin/email') }}">Email</a></li>
          @endif
-         @if (sidebarGuard(19))
-            <li class="m-t"><a href="{{ url('admin/payment') }}">Payment gateway</a></li>
-         @endif
          @if (sidebarGuard(18))
             <li class="m-t"><a href="{{ url('admin/settings') }}">Site settings</a></li>
          @endif
@@ -76,9 +73,6 @@
            @endif
            @if (sidebarGuard(3))
            <li class="m-t"><a href="{{ url('admin/email') }}">Email</a></li>
-           @endif
-           @if (sidebarGuard(19))
-               <li class="m-t"><a href="{{ url('admin/payment') }}">Payment gateway</a></li>
            @endif
            @if (sidebarGuard(18))
            <li class="m-t"><a href="{{ url('admin/settings') }}">Site settings</a></li>
