61fb810d22d009ec877c2d69f7c0c9f80e37f182
ignitedcms <iamthwee@gmail.com>
2024-02-05 20:20:44 +0000

    Update sidebar.blade.php

--- src/views/admin/sidebar.blade.php
+++ src/views/admin/sidebar.blade.php
@@ -5,36 +5,36 @@
         </a>
         <ul class="rm-list-styles">
          @if (sidebarGuard(6))
-            <li class="mt-3"><a href="{{ url('admin/profile') }}">Profile</a></li>
+            <li class="mt-3 hover:text-gray-900"><a href="{{ url('admin/profile') }}">Profile</a></li>
          @endif
          @if (sidebarGuard(9))
-            <li class="mt"><a href="{{ url('admin/users') }}">Users</a></li>
+            <li class="mt hover:text-gray-900"><a href="{{ url('admin/users') }}">Users</a></li>
          @endif
          @if (sidebarGuard(5))
-            <li class="mt"><a href="{{ url('admin/permissions') }}">Permissions</a></li>
+            <li class="mt hover:text-gray-900"><a href="{{ url('admin/permissions') }}">Permissions</a></li>
          @endif
          @if (sidebarGuard(13))
-            <li class="mt"><a href="{{ url('admin/fields') }}">Fields</a></li>
+            <li class="mt hover:text-gray-900"><a href="{{ url('admin/fields') }}">Fields</a></li>
          @endif
          @if (sidebarGuard(14))
-            <li class="mt"><a href="{{ url('admin/section') }}">Sections</a></li>
+            <li class="mt hover:text-gray-900"><a href="{{ url('admin/section') }}">Sections</a></li>
          @endif
          @if (sidebarGuard(15))
-            <li class="mt"><a href="{{ url('admin/entry') }}">Entries</a></li>
+            <li class="mt hover:text-gray-900"><a href="{{ url('admin/entry') }}">Entries</a></li>
          @endif
          @if (sidebarGuard(17))
-            <li class="mt"><a href="{{ url('admin/assets') }}">Assets</a></li>
+            <li class="mt hover:text-gray-900"><a href="{{ url('admin/assets') }}">Assets</a></li>
          @endif
          @if (sidebarGuard(10))
-            <li class="mt"><a href="{{ url('admin/database') }}">Database</a></li>
+            <li class="mt hover:text-gray-900"><a href="{{ url('admin/database') }}">Database</a></li>
          @endif
          @if (sidebarGuard(3))
-            <li class="mt"><a href="{{ url('admin/email') }}">Email</a></li>
+            <li class="mt hover:text-gray-900"><a href="{{ url('admin/email') }}">Email</a></li>
          @endif
          @if (sidebarGuard(18))
-            <li class="mt"><a href="{{ url('admin/settings') }}">Site settings</a></li>
+            <li class="mt hover:text-gray-900"><a href="{{ url('admin/settings') }}">Site settings</a></li>
          @endif
-            <li class="mt inline">
+            <li class="mt inline hover:text-gray-900">
                <form action="{{ url('logout') }} " method="POST">
                   @csrf
                   <button type="submit" class="rm-btn-styles">Logout</button>
